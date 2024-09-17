f35492de7c3951f24cc85a737615d91660bcfb76
Anamika <anamikadobriyal21@gmail.com>
2024-09-18 01:18:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 //your JS code here. If required.
 const inputs = document.querySelectorAll('.code');
-inputs.forEach((input, index) => {
-    input.addEventListener('keydown', (e) => {
-        if (e.key >= 0 && e.key <= 9) {
-            inputs[index].value = ''; // Clear input for next digit
-            setTimeout(() => {
-                if (index < inputs.length - 1) {
-                    inputs[index + 1].focus(); // Focus next input
-                }
-            }, 100);
-        } else if (e.key === 'Backspace') {
-            if (index > 0) {
-                setTimeout(() => {
-                    inputs[index - 1].focus(); // Focus previous input
-                }, 100);
-            }
-        }
-    });
-});
+inputs.forEach((input, index)=> {
+	input.addEventListener('input',() => {
+		if(input.value.length === 1 && index < inputs.length - 1)
+		{
+			inputs[index + 1].focus();
+		}
+	});
+
+     input.addEventListener('keydown',(event) => {
+		 if(event.key === 'Backspace' && input.value === ''){
+			 if(index>0)
+			 {
+				 inputs[index - 1].focus();
+				 inputs[index - 1].value = '';
+			 }
+		 }
+	 })
+})
